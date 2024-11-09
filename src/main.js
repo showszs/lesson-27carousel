0aa13bac0751a250a410fb47976c6d31eab99088
showszs <pasativan152@gmail.com>
2024-11-09 21:52:15 +0300

    lesson27upd

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-(function () {
+;(function () {
   const container = document.querySelector('#carousel')
   const slides = container.querySelectorAll('.slide')
   const indicatorsContainer = container.querySelector('#indicators-container')
@@ -13,104 +13,104 @@
   const FA_PAUSE = '<i class="fas fa-pause-circle"></i>'
   const FA_PLAY = '<i class="fas fa-play-circle"></i>'
   const INTERVAL = 2000
-  
+
   let currentSlide = 0
   let isPlaying = true
   let timerId = null
   let startPosX = null
   let endPosX = null
-  
-  function gotoSlide(n){
-    slides[currentSlide].classList.toggle('active') 
-    indicatorItems[currentSlide].classList.toggle('active') 
+
+  function gotoSlide(n) {
+    slides[currentSlide].classList.toggle('active')
+    indicatorItems[currentSlide].classList.toggle('active')
     currentSlide = (n + slides.length) % slides.length
-    slides[currentSlide].classList.toggle('active') 
-    indicatorItems[currentSlide].classList.toggle('active') 
+    slides[currentSlide].classList.toggle('active')
+    indicatorItems[currentSlide].classList.toggle('active')
   }
-  
+
   function gotoPrev() {
     gotoSlide(currentSlide - 1)
   }
-  
+
   function gotoNext() {
-   gotoSlide(currentSlide + 1)
+    gotoSlide(currentSlide + 1)
   }
-  
+
   function tick() {
-    timerId = setInterval( gotoNext, INTERVAL)
+    timerId = setInterval(gotoNext, INTERVAL)
   }
-  
+
   function pauseHandler() {
     if (!isPlaying) return
     pauseBtn.innerHTML = FA_PLAY
     clearInterval(timerId)
     isPlaying = !isPlaying
   }
-  
+
   function playHandler() {
     pauseBtn.innerHTML = FA_PAUSE
     tick()
     isPlaying = true
   }
-  
-  function pausePlayHandler(){
+
+  function pausePlayHandler() {
     isPlaying ? pauseHandler() : playHandler()
   }
-  
+
   function prevHandler() {
     pauseHandler()
     gotoPrev()
   }
-  
+
   function nextHandler() {
     pauseHandler()
     gotoNext()
   }
 
-  function indicateHandler (e) {
-   const { target } = e
-   if (target && target.classList.contains('indicator')) {
-     pauseHandler()
-     gotoSlide(+target.dataset.slideTo)
-   }
+  function indicateHandler(e) {
+    const { target } = e
+    if (target && target.classList.contains('indicator')) {
+      pauseHandler()
+      gotoSlide(+target.dataset.slideTo)
+    }
   }
-  
-  function pressKeyHandler(e){
-  const { code } = e
-  if (code === CODE_ARROW_LEFT) prevHandler()
-  if (code === CODE_ARROW_RIGHT) nextHandler()
-  if (code === CODE_SPACE) {
-    e.preventDefault()
-    pausePlayHandler()
-  }
-  }
-  
-  function swipeStartHandler(e) {
-  startPosX = e instanceof MouseEvent ? e.pageX : e.changedTouches[0].pageX
-  }
-  
-  function swipeEndHandler(e) {
-   endPosX = e instanceof MouseEvent ? e.pageX : e.changedTouches[0].pageX
-    if (endPosX - startPosX > 100) prevHandler()
-    if (endPosX - startPosX < -100) nextHandler() 
-  }
-  
-function initListeners() {
-  pauseBtn.addEventListener('click', pausePlayHandler)
-  nextBtn.addEventListener('click', nextHandler)
-  prevBtn.addEventListener('click', prevHandler)
-  indicatorsContainer.addEventListener('click', indicateHandler)
-  container.addEventListener('touchstart', swipeStartHandler)
-  container.addEventListener('mousedown', swipeStartHandler)
-  container.addEventListener('touchend', swipeEndHandler)
-  container.addEventListener('mouseup', swipeEndHandler)
-  document.addEventListener('keydown', pressKeyHandler)
-} 
 
-function init() {
+  function pressKeyHandler(e) {
+    const { code } = e
+    if (code === CODE_ARROW_LEFT) prevHandler()
+    if (code === CODE_ARROW_RIGHT) nextHandler()
+    if (code === CODE_SPACE) {
+      e.preventDefault()
+      pausePlayHandler()
+    }
+  }
+
+  function swipeStartHandler(e) {
+    startPosX = e instanceof MouseEvent ? e.pageX : e.changedTouches[0].pageX
+  }
+
+  function swipeEndHandler(e) {
+    endPosX = e instanceof MouseEvent ? e.pageX : e.changedTouches[0].pageX
+    if (endPosX - startPosX > 100) prevHandler()
+    if (endPosX - startPosX < -100) nextHandler()
+  }
+
+  function initListeners() {
+    pauseBtn.addEventListener('click', pausePlayHandler)
+    nextBtn.addEventListener('click', nextHandler)
+    prevBtn.addEventListener('click', prevHandler)
+    indicatorsContainer.addEventListener('click', indicateHandler)
+    container.addEventListener('touchstart', swipeStartHandler)
+    container.addEventListener('mousedown', swipeStartHandler)
+    container.addEventListener('touchend', swipeEndHandler)
+    container.addEventListener('mouseup', swipeEndHandler)
+    document.addEventListener('keydown', pressKeyHandler)
+  }
+
+  function init() {
     initListeners()
     tick()
   }
 
-init()
-}())
+  init()
+})()
